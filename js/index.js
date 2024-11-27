3dc66ba1677fdfd8002ff51a5dd5bba2b131d10c
0mar-cc <omarmoh3010@gmail.com>
2024-11-27 05:17:34 +0200

    fixed displaying login card

--- js/index.js
+++ js/index.js
@@ -26,6 +26,7 @@ function displayLoginCard(){
 
 displaySignUp.addEventListener('click', displaySignUpCard)
 
+displayLogin.addEventListener('click', displayLoginCard)
 
 const signature = document.querySelector('.signature')
 
