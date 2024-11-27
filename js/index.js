de64ef8fb91567b284d228a778e063555ecda236
0mar-cc <omarmoh3010@gmail.com>
2024-11-27 06:12:32 +0200

    Fixed the font size on different screens

--- js/index.js
+++ js/index.js
@@ -1,36 +1,27 @@
-let displaySignUp = document.getElementById('displaySignUp')
-let displayLogin = document.getElementById('displayLogin')
+let displaySignUp = document.getElementById("displaySignUp");
+let displayLogin = document.getElementById("displayLogin");
 
-let createAccountCard = document.getElementById('createAccountCard')
-let loginCard = document.getElementById('loginCard')
+let createAccountCard = document.getElementById("createAccountCard");
+let loginCard = document.getElementById("loginCard");
 
-
-
-
-function displaySignUpCard(event){
-    event.preventDefault();
-    event.stopPropagation();
-    createAccountCard.classList.remove('d-none');
-    loginCard.classList.add('d-none')
+function displaySignUpCard(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  createAccountCard.classList.remove("d-none");
+  loginCard.classList.add("d-none");
 }
 
-
-function displayLoginCard(){
-    event.preventDefault();
-    event.stopPropagation();
-    loginCard.classList.remove('d-none');
-    createAccountCard.classList.add('d-none')
+function displayLoginCard() {
+  event.preventDefault();
+  event.stopPropagation();
+  loginCard.classList.remove("d-none");
+  createAccountCard.classList.add("d-none");
 }
 
+displaySignUp.addEventListener("click", displaySignUpCard);
 
+displayLogin.addEventListener("click", displayLoginCard);
 
+const signature = document.querySelector(".signature");
 
-displaySignUp.addEventListener('click', displaySignUpCard)
-
-displayLogin.addEventListener('click', displayLoginCard)
-
-const signature = document.querySelector('.signature')
-
-signature.innerText = signature.innerText
-  .split('')
-  .join('\n')
+signature.innerText = signature.innerText.split("").join("\n");
