3680a144042011e6761daa15848a4f7a62134c4d
0mar-cc <omarmoh3010@gmail.com>
2024-11-27 04:55:09 +0200

    Completed page design for all screens

---
+++ js/index.js
@@ -0,0 +1,34 @@
+let displaySignUp = document.getElementById('displaySignUp')
+let displayLogin = document.getElementById('displayLogin')
+
+let createAccountCard = document.getElementById('createAccountCard')
+let loginCard = document.getElementById('loginCard')
+
+
+
+
+
+function displaySignUpCard(event){
+    event.preventDefault();
+    event.stopPropagation();
+    createAccountCard.classList.remove('d-none');
+    loginCard.classList.add('d-none')
+}
+
+
+function displayLoginCard(){
+    loginCard.classList.remove('d-none');
+    createAccountCard.classList.add('d-none')
+}
+
+
+
+
+displaySignUp.addEventListener('click', displaySignUpCard)
+
+
+const signature = document.querySelector('.signature')
+
+signature.innerText = signature.innerText
+  .split('')
+  .join('\n')
